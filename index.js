9798286867952a01c18c3e0445019ee35e0501f6
Julio Barrios <julio.barrios.9618@gmail.com>
2022-07-14 23:22:37 -0300

    Deploy

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Mi server en Express');
+});
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Nueva ruta');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
